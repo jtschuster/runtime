a2f38e6ed0b865456972ebe3af54be3c1ed6707c
Jackson Schuster <jschuster@microsoft.com>
2023-03-08 12:13:44 -0800

    Add tests

--- src/libraries/System.Runtime.InteropServices/tests/ComInterfaceGenerator.Tests/ComInterfaceDefinitions.cs
+++ src/libraries/System.Runtime.InteropServices/tests/ComInterfaceGenerator.Tests/ComInterfaceDefinitions.cs
@@ -2,59 +2,65 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Collections;
+using System.Linq;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.Marshalling;
 using Xunit;
+using Xunit.Sdk;
 
-namespace ComInterfaceGenerator.Tests
+namespace ComInterfaceGenerator.Tests;
+
+[GeneratedComInterface]
+[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+[Guid("2c3f9903-b586-46b1-881b-adfce9af47b1")]
+public partial interface IComInterface
 {
-
-    [GeneratedComInterface]
-    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-    [Guid("2c3f9903-b586-46b1-881b-adfce9af47b1")]
-    public partial interface IComInterface
-    {
-        void Method1();
-        int Method2();
-    }
-
-    public class ImplIComInterface : IComInterface
-    {
-        int _data = 0;
-        public void Method1() => _data = 2;
-        public int Method2() => _data;
-    }
-internal sealed partial class MyGeneratedComWrappers
-    {
-    }
-
-
-    public class Tests
-    {
-#pragma warning disable xUnit1004 // Test methods should not be skipped
-        //[Fact(Skip = "Not Implemented Yet")]
-#pragma warning restore xUnit1004 // Test methods should not be skipped
-        [Fact]
-        public unsafe void UseGeneratedComInterface()
-        {
-            var ob = new[] { new ImplIComInterface()};
-            var vtable = asdf<IComInterface>();
-#pragma warning disable CS8500 // This takes the address of, gets the size of, or declares a pointer to a managed type
-            fixed (void* ptr = &ob[0])
-            {
-                var data = ((delegate*<void*, int>)vtable[4])(ptr);
-                ((delegate*<void*, void>)vtable[3])(ptr);
-                var data2 = ((delegate*<void*, int>)vtable[4])(ptr);
-            }
-#pragma warning restore CS8500 // This takes the address of, gets the size of, or declares a pointer to a managed type
-            //throw new NotImplementedException("Not Implemented");
-        }
-
-        internal unsafe void** asdf<T>() where T: IIUnknownInterfaceType
-        {
-            return T.ManagedVirtualMethodTable;
-        }
-    }
+    void Method1();
+    int Method2();
 }
 
+public class ImplIComInterface : IComInterface
+{
+    int _data = 0;
+    public void Method1() => _data = 2;
+    public int Method2() => _data;
+}
+internal sealed partial class MyGeneratedComWrappers : ComWrappers
+{
+    protected override unsafe ComInterfaceEntry* ComputeVtables(object obj, CreateComInterfaceFlags flags, out int count) => throw new NotImplementedException();
+    protected override object? CreateObject(nint externalComObject, CreateObjectFlags flags) => throw new NotImplementedException();
+    protected override void ReleaseObjects(IEnumerable objects) => throw new NotImplementedException();
+}
+
+
+public class Tests
+{
+    [Fact]
+    public unsafe void UseGeneratedComInterface()
+    {
+        var details = typeof(IComInterface).GetCustomAttributes().Where(attr => attr.GetType().Name == "IUnknownDerivedAttribute`2").Single();
+        var createManagedVirtualFunctionTable = details.GetType().GenericTypeArguments[1].GetRuntimeMethods().Where(x => x.Name == "CreateManagedVirtualFunctionTable").Single(); // details.GetType().GenericTypeArguments[1].GetRuntimeMethod("CreateManagedVirtualFunctionTable", new Type[] { });
+        var ret = createManagedVirtualFunctionTable.Invoke(null, null);
+        var vtable = (void**)Pointer.Unbox(createManagedVirtualFunctionTable.Invoke(null, null));
+
+        var test = new ImplIComInterface();
+
+        MyGeneratedComWrappers.TryGetComInstance(test, out var asdf);
+        //GCHandle gcHandle = GCHandle.Alloc(test, GCHandleType.WeakTrackResurrection);
+        //IntPtr ptr = Marshal.ReadIntPtr(GCHandle.ToIntPtr(gcHandle));
+        //if (!MyGeneratedComWrappers.TryGetComInstance(test, out var ptr))
+        //    throw new FailException("Didn't work");
+        var ptr = Unsafe.AsPointer(ref test);
+        var data = ((delegate* unmanaged<void*, int>)(vtable[4]))((void*)ptr);
+        Assert.Equal(0, data);
+        ((delegate* unmanaged<void*, void>)(vtable[3]))((void*)ptr);
+        Assert.Equal(2, test.Method2());
+        var data2 = ((delegate* unmanaged<void*, int>)(vtable[4]))((void*)ptr);
+        Assert.Equal(2, data2);
+
+
+    }
+}
