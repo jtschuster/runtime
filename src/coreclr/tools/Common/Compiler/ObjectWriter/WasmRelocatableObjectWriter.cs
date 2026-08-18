e3a4debc6925d69c25933ada4a814d9168c23e4c
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2026-08-17 21:21:18 -0700

    Move Wasm Function section emission to node graph
    - Create WasmFunctionEntryNode to represent an entry in the Function section.
      - These emit relocations to the index of the wasm type / signature index for
        the method.
      - They use the represented method as the key for sorting to ensure the order
        of the function section matches the order of the method code nodes in the
        code section.
      - These are rooted as dependencies of all ObjectNodes that implement
        INodeWithTypeSignature. Funclets also get their own and fit into the
        sorting to ensure they are placed in order.
    - Create WebcilDefaultMethodNode to represent a webcil spec method that must be
      exported.
      - These are implemented as assembly stubs.
      - They are emitted in the Ordered phase to ensure they are emitted first, and
        sort properly between themselves.
      - fillWebcilTable requires a const for the method count of the module, so a
        new reloc WASM_FUNCTION_COUNT_SLEB is added.
      - The three methods are rooted in crossgen wasm compilations.
    - Tests validate that the WASM_FUNCTION_COUNT_SLEB relocation is
      accurate.
    - Validated crossgen test modules with wasm-validate.
    - ILCompiler also has these changes, but full wasm modules are not yet
      emitted, so this is untested.

--- src/coreclr/tools/Common/Compiler/ObjectWriter/WasmRelocatableObjectWriter.cs
+++ src/coreclr/tools/Common/Compiler/ObjectWriter/WasmRelocatableObjectWriter.cs
@@ -81,6 +81,7 @@ namespace ILCompiler.ObjectWriter
                 fixed (byte* pData = ReadRelocToDataSpan(reloc, relocScratchBuffer, sectionStart))
                 {
                     long addend = Relocation.ReadValue(reloc.Type, pData);
+                    Debug.Assert(addend == reloc.Addend);
                     int relocLength = Relocation.GetSize(reloc.Type);
 
                     switch (reloc.Type)
